0a7372e6c5395110214824619483efa656db5b65
Sushmita67 <biswakarmasushmita5@gmail.com>
2025-06-18 18:17:34 +0545

    Image Controller

---
+++ backend/controllers/imageController.js
@@ -0,0 +1,14 @@
+
+const uploadImage = async (req, res, next) => {
+
+
+    if (!req.file) {
+        return res.status(400).send({ message: "Please upload a file" });
+    }
+    res.status(200).json({
+        success: true,
+        data: req.file.filename,
+    });
+}
+
+module.exports={uploadImage};
